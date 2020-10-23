a6bf7e0878eca27ff7e8f8222a36116896185e7e
heckfy <ruslan.k.yakhin@gmail.com>
2020-10-23 12:41:52 +0200

    app/main.py

--- app/utils/mqtt.py
+++ app/utils/mqtt.py
@@ -4,6 +4,8 @@ import paho.mqtt.publish as publish
 client = False
 
 def mqtt_publish(host, user, password, topic, message):
+    global client
+
     if not client:
         client = mqtt.Client("ha-client")
         client.username_pw_set(user, password)
