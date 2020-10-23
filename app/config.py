225ff7b59c361b3dc2db2baeec424395b00ded42
heckfy <ruslan.k.yakhin@gmail.com>
2020-10-23 13:05:05 +0200

    app/config.py

--- app/config.py
+++ app/config.py
@@ -15,3 +15,8 @@ MQTT_TOPIC = os.getenv('MQTT_TOPIC')
 MQTT_HOST = os.getenv('MQTT_HOST')
 MQTT_USER = os.getenv('MQTT_USER')
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
+
+MQTT_ENABLE = True if MQTT_TOPIC \
+    and MQTT_HOST \
+    and MQTT_USER \
+    and MQTT_PASSWORD else False
