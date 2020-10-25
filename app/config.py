5e5d83ca1cf4b156aa2fb62c30314183b5c9ed59
heckfy <ruslan.k.yakhin@gmail.com>
2020-10-25 15:56:30 +0100

    Entity name

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ SQS_QUEUE = os.getenv('SQS_QUEUE')
 AWS_REGION = os.getenv('AWS_REGION')
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
-ENTITY_ID = os.getenv('ENTITY_ID', 'sqs.message_body')
+ENTITY_ID = os.getenv('ENTITY_ID')
 
 MQTT_TOPIC = os.getenv('MQTT_TOPIC')
 MQTT_HOST = os.getenv('MQTT_HOST')
