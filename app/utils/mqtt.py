e1911089d61a1b8018dae88fcfea78c28a2dbc07
heckfy <ruslan.k.yakhin@gmail.com>
2020-10-25 18:17:05 +0100

    Single for mqtt

--- app/utils/mqtt.py
+++ app/utils/mqtt.py
@@ -5,15 +5,25 @@ import paho.mqtt.publish as publish
 client = False
 
 def mqtt_publish(host, user, password, topic, message):
-    global client
+    # global client
 
-    if not client:
-        client = mqtt.Client()
-        client.username_pw_set(user, password)
-        client.enable_logger(logging.getLogger(__name__))
-        client.connect(host)
+    # if not client:
+    #     client = mqtt.Client()
+    #     client.username_pw_set(user, password)
+    #     client.enable_logger(logging.getLogger(__name__))
+    #     client.connect(host)
 
-    if client.publish(topic, message):
-        logging.info(f'Published topic {topic}.')
+    # if client.publish(topic, message):
+    #     logging.info(f'Published topic {topic}.')
 
-    return client
+    # return client
+    publish.single(
+        topic,
+        message,
+        hostname=host,
+        auth={
+            'username': user,
+            'password': password
+        },
+        keepalive=120
+    )
